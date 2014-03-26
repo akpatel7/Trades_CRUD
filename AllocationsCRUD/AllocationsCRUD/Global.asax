9e6ba20102ac98258cf1443856dc4d6f1416dfec
apatel7886 <msg.amit.patel@gmail.com>
2014-03-26 13:31:10 -0400

    BOWA (Breeze, OData with Angular) SPA template (raw)

---
+++ AllocationsCRUD/AllocationsCRUD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AllocationsCRUD.MvcApplication" Language="C#" %>
---
+++ AllocationsCRUD/AllocationsCRUD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AllocationsCRUD.Startup))]
+namespace AllocationsCRUD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
