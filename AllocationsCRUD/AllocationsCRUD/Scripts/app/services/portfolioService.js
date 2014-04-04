6c7fdb8154c2fa5e2ab38d2f197b0165901c476a
apatel7886 <msg.amit.patel@gmail.com>
2014-04-04 13:43:42 -0400

    Restructure js files, manual bootstrap of Angularjs. Todo: load portfolio-detail as partial view.

---
+++ AllocationsCRUD/AllocationsCRUD/Scripts/app/services/portfolioService.js
@@ -0,0 +1,53 @@
+'use strict';
+
+tradesApp.factory('allocationsService', function ($resource, $q) {
+    var resource = $resource('/odata/:entity?:odataParam', { entity: '@entity', odataParam: '@odataParam' });
+        return {
+        getAllocationSummaries: function (odataParam) {
+            var deferred = $q.defer();
+            resource.get({ entity: 'AllocationSummaries', odataParam: odataParam },
+                function (data) {
+                    deferred.resolve(data);
+                },
+                function (response) {
+                    deferred.reject(response);
+                });
+            return deferred.promise;
+        },
+        getPortfolioSummaries: function (odataParam) {
+            var deferred = $q.defer();
+            resource.get({ entity: 'PortfolioSummaries', odataParam: odataParam },
+                function (data) {
+                    deferred.resolve(data);
+                },
+                function (response) {
+                    deferred.reject(response);
+                });
+            return deferred.promise;
+        },
+        getAllocationHistories: function (allocationId, odataParam) {
+            var deferred = $q.defer();
+            resource.get({ entity: 'AllocationHistorySummaries(' + allocationId + ')/', odataParam: odataParam },
+                function (data) {
+                    deferred.resolve(data);
+                },
+                function (response) {
+                    deferred.reject(response);
+                });
+            return deferred.promise;
+        },
+        
+        getCommentSummaries: function (portfolioId, odataParam) {
+        var deferred = $q.defer();
+        resource.get({ entity: 'CommentSummaries(' + portfolioId + ')', odataParam: odataParam },
+            function (data) {
+                deferred.resolve(data);
+            },
+            function (response) {
+                deferred.reject(response);
+            });
+        return deferred.promise;
+    }
+    };
+});
+
